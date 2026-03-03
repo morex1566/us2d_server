e9beaeaec7bf7277d13a3ab8413c1f218b9ad717
Jiwon <morex1566@gmail.com>
2026-03-04 03:02:11 +0900

    버그 수정 : 링버퍼

--- src/net.common/ts_ring.h
+++ src/net.common/ts_ring.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <atomic>
+#include <vector>
 
 namespace net::common
 {
+    /// <summary>
+    /// lock-free MPMC 링버퍼. push/pop 모두 CAS 기반으로 thread-safe
+    /// </summary>
     template <typename t>
     class ts_ring
     {
@@ -15,7 +20,7 @@ namespace net::common
         };
 
     public:
-        ts_ring(size_t size) : size(size), head(0), tail(0)
+        ts_ring(size_t size) : size(size), front(0), back(0)
         {
             t_buffer.resize(size);
         }
@@ -29,81 +34,85 @@ namespace net::common
             // std::vector가 자동 해제
         }
 
+        /// <summary>
+        /// 링버퍼에 데이터를 추가. 버퍼가 꽉 찬 경우 false 반환
+        /// </summary>
         bool push(const t& in)
         {
-            size_t curr_head_idx = head.load(std::memory_order_relaxed);
-            size_t next_head_idx;
-            const auto capacity = size.load(std::memory_order_acquire);
-
             // 작업 공간 인덱스 획득 과정
             while (true)
             {
-                next_head_idx = (curr_head_idx + 1) % capacity;
+                size_t curr_front_idx = front.load(std::memory_order_relaxed);
+                size_t next_front_idx = (curr_front_idx + 1) % capacity;
+                const auto capacity = size.load(std::memory_order_acquire);
 
                 // 실패 : 버퍼 꽉 참
-                if (next_head_idx == tail.load(std::memory_order_acquire))
+                if (next_front_idx == back.load(std::memory_order_acquire))
                 {
                     return false;
                 }
 
-                // 성공 : current_head이 인덱스 get
-                if (head.compare_exchange_weak(curr_head_idx, next_head_idx,
+                // 성공 : CAS 전 write 인덱스를 별도 보관 (CAS 성공 시 expected가 변경됨)
+                size_t write_idx = curr_front_idx;
+                if (front.compare_exchange_weak(curr_front_idx, next_front_idx,
                     std::memory_order_release,
                     std::memory_order_relaxed))
                 {
-                    break;
+                    t_buffer[write_idx].data = std::move(in);
+                    t_buffer[write_idx].is_ready.store(true, std::memory_order_release);
+                    return true;
                 }
             }
-
-            t_buffer[curr_head_idx].data = std::move(in);
-            t_buffer[curr_head_idx].is_ready.store(true, std::memory_order_release);
-
-            return true;
         }
 
+        /// <summary>
+        /// 링버퍼에서 데이터를 꺼냄. 버퍼가 비어있거나 데이터가 준비 안된 경우 false 반환
+        /// </summary>
         bool pop(t& out)
         {
-            size_t curr_tail_idx = tail.load(std::memory_order_relaxed);
-            size_t next_tail_idx;
-            const auto capacity = size.load(std::memory_order_acquire);
-
             // 작업 공간 인덱스 획득 과정
             while (true)
             {
+                size_t curr_back_idx = back.load(std::memory_order_relaxed);
+                size_t next_back_idx;
+                const auto capacity = size.load(std::memory_order_acquire);
+
                 // 실패 : 버퍼 비었음
-                if (curr_tail_idx == head.load(std::memory_order_acquire))
+                if (curr_back_idx == front.load(std::memory_order_acquire))
                 {
                     return false;
                 }
 
                 // 실패 : 데이터가 아직 안들어왔음
-                if (t_buffer[curr_tail_idx].is_ready.load(std::memory_order_acquire) == false)
+                if (t_buffer[curr_back_idx].is_ready.load(std::memory_order_acquire) == false)
                 {
                     return false;
                 }
 
-                next_tail_idx = (curr_tail_idx + 1) % capacity;
+                next_back_idx = (curr_back_idx + 1) % capacity;
 
-                // 성공 : current_tail이 인덱스 get
-                if (tail.compare_exchange_weak(curr_tail_idx, next_tail_idx,
+                // 성공 : CAS 전 read 인덱스를 별도 보관 (CAS 성공 시 expected가 변경됨)
+                size_t read_idx = curr_back_idx;
+                if (back.compare_exchange_weak(curr_back_idx, next_back_idx,
                     std::memory_order_release,
                     std::memory_order_relaxed))
                 {
-                    break;
+                    out = std::move(t_buffer[read_idx].data);
+                    t_buffer[read_idx].is_ready.store(false, std::memory_order_release);
+                    return true;
                 }
             }
-
-            out = std::move(t_buffer[curr_tail_idx].data);
-            t_buffer[curr_tail_idx].is_ready.store(false, std::memory_order_release);
-
-            return true;
         }
 
+        /// <summary>
+        /// 링버퍼 최대 크기 반환
+        /// </summary>
+        inline size_t capacity() const { return size.load(std::memory_order_relaxed); }
 
     private:
         std::vector<node> t_buffer;
         std::atomic<size_t> size;
-        std::atomic<size_t> head;
-        std::atomic<size_t> tail;
+        std::atomic<size_t> front;
+        std::atomic<size_t> back;
     };
 }
